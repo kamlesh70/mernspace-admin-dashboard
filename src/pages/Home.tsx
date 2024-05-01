fda7a29b8f8687af17ddbee07b9450de3efe31ac
kamlesh70 <kamalmehra3571@gmail.com>
2024-05-01 20:32:28 +0530

    add prettier

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
 const Home = () => {
     return (
         <div>
-            <h1>This is home page</h1>
+            
+                                                               
+                                                                     
+                                                                          <h1>This is home page</h1>
         </div>
     )
 }
